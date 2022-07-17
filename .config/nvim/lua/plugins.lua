6e7404ed0b9b5d3b2db759fbd68af7965ce7715c
Arhan Jain <arhan.j04@gmail.com>
2022-07-17 00:22:47 -0700

    full nvim setup

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,29 +1,21 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
-
 local fn = vim.fn
-
-
-vim.g.package_home = fn.stdpath("data") .. "/site/pack/packer/"
-local packer_install_dir = vim.g.package_home .. "/opt/packer.nvim"
-
-local packer_repo = "https://github.com/wbthomason/packer.nvim"
-local install_cmd = string.format("10split |term git clone --depth=1 %s %s", packer_repo, packer_install_dir)
-
--- Auto-install packer in case it hasn't been installed.
-if fn.glob(packer_install_dir) == "" then
-  vim.api.nvim_echo({ { "Installing packer.nvim", "Type" } }, true, {})
-  vim.cmd(install_cmd)
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local new_install = false
+if fn.empty(fn.glob(install_path)) > 0 then
+  new_install = "true"
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+vim.o.runtimepath = vim.fn.stdpath('data') .. '/site/pack/*/start/*,' .. vim.o.runtimepath
 end
 
--- Load packer.nvim
-vim.cmd("packadd packer.nvim")
+
+--vim.cmd[[packadd packer.nvim]]
 
 return require("packer").startup(function()
 	-- Speed up startup time
-	use {'lewis6991/impatient.nvim', config = [[require('impatient')]]}
+--	use {'lewis6991/impatient.nvim', config = [[require('impatient')]]}
 
 	-- Packer manages itself
-	use {'wbthomason/packer.nvim', opt = true}
+	use {'wbthomason/packer.nvim'}
 
 	-- LSP Kind: Pictograms for completion
 	use {"onsails/lspkind-nvim", event = "VimEnter"}
@@ -59,7 +51,9 @@ return require("packer").startup(function()
 	use { 'projekt0n/github-nvim-theme', opt = true }
 	use {'EdenEast/nightfox.nvim'}
 	
-
+  if new_install then
+    require('packer').sync()
+  end
 end)
 
 		
