9a6d2427054c4debc8f8b70e3252a2df088f176d
Arhan Jain <arhan.j04@gmail.com>
2022-08-12 15:03:47 -0700

    scaling issue fix

--- .config/i3/scaling.sh
+++ .config/i3/scaling.sh
@@ -1,6 +1,6 @@
 xrandr --dpi 192
 
-if [ xrandr | grep -sw 'connected' | wc -l > 1 ]; then
-  xrandr --output DP-1 --primary --scale 1.25x1.25 --mode 3840x1600 --pos 0x0 \
-  --output eDP-1 --mode 2736x1824 --pos 4800x0
+if [ $(xrandr | grep -sw 'connected' | wc -l) -gt 1 ]; then
+  xrandr --output DP-1 --scale 1.3x1.3 --mode 3840x1600 --pos 0x0 \
+  --output eDP-1 --mode 2736x1824 --pos 4992x0
 fi
